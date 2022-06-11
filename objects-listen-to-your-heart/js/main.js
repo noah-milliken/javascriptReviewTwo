f17a19fab3947a546a77d9ba5e3f090e0a606fa8
noah milliken <noahmilliken@gmail.com>
2022-06-11 13:53:13 -0400

    literal objects

--- objects-listen-to-your-heart/js/main.js
+++ objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,17 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+
+stopwatch.color = 'black'
+stopwatch.shape = 'round'
+stopwatch.brand = 'nike'
+stopwatch.smell = 'new'
+
+stopwatch.start = function(){
+     console.log('GOOO!')
+}
+stopwatch.stop = function(){
+    console.log('Stop')
+}
+stopwatch.morph = function(){
+    console.log('its morphing time')
+}
