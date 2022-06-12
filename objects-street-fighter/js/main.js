85f003d4a61178ee8359695d7c289d9bb78023b5
noah milliken <noahmilliken@gmail.com>
2022-06-12 18:25:23 -0400

    more practice

--- objects-street-fighter/js/main.js
+++ objects-street-fighter/js/main.js
@@ -1,1 +1,19 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a pizza constructor that makes fighting game characters with 4 properties and 3 methods
+function streetFighter(name,health,speed,specialMove){
+    this.name = name
+    this.health = health
+    this.speed = speed
+    this.specialMove = specialMove
+
+    this.throq = function(){
+        console.log('Yeet')
+    }
+    this.taunt = function(){
+        console.log(`You cant handel my $(this.specialMove)`)
+    }
+    this.intro = function(){
+        console.log(`Hello i am ${this.name}`)
+    }
+}
+
+let bigNoNo = new streetFighter('Big NO-NO',150,15,'Yummy Gummy')
--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,67 @@
+for(let i = 0; i < 5; i++){
+    console.log(`I like to eat ${i} pieces of pizza`)
+}
 
+for(let i = 10; i>= 1; i--){
+    console.log(`I ran ${i} miles ${i+100} days ago`);
+    
+}
+let x = 0;
+while(x<5){
+    console.log('bananas are great for cats')
+    x++;
+}
+//do while loops should be used if you want to do something at least once.
+
+let y = 10
+do{
+    console.log(x)
+    y++;
+}while (x < 5);
+
+//math
+
+console.log(Math.ceil((Math.random())*10))
+console.log(Math.floor((Math.random())*10))
+
+
+function squareArea(radius){
+    console.log(2*Math.PI*radius^2)
+}
+squareArea(34)
+
+function modulus(n1,n2){
+    console.log(n1 % n2)
+}
+modulus(34,56)
+
+
+let z = 45
+let a = z*z
+console.log(z);
+console.log(a)
+
+
+let obj = {
+    name: 'horace',
+    married: true,
+    'mother\'s Name': 'Alice',
+    'year of birth': 1234,
+    getAge: function(){
+        return 1234 - obj['year of birth']
+    },
+    1: 'one'
+
+};
+
+'original string'.replace('original', 'replaced');
+
+let originaString = 'original string'
+originaString = originaString.replace('original', 'barfing cats');
+console.log(originaString)
+
+
+let num1 = 5;
+let num2 = num1 - 2;
+let num3 = num1 % num2;
+console.log(num1+num2+num3);
